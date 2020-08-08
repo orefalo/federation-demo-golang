810dfa57336cc0f187bd491d007ce1e9bb379e9e
Olivier Refalo <orefalo@snapcore.com>
2020-08-08 16:10:18 +0400

    fix

--- services_ts/gateway/index.ts
+++ services_ts/gateway/index.ts
@@ -8,6 +8,7 @@ const gateway = new ApolloGateway({
     { name: "reviews", url: "http://localhost:4002" },
     { name: "products", url: "http://localhost:4003" },
     { name: "inventory", url: "http://localhost:4004" },
+    { name: "chat", url: "http://localhost:4005" },
   ],
   debug: true,
 
